bdefacd390f0cadfe892fc16ec12aa738c2770a3
renatasarmet <renatassmendes@hotmail.com>
2018-11-25 19:51:45 -0200

    Mapa funcionando!

--- app/src/main/java/com/example/mariana/projetomds/fragments/mapa/MapaActivity.java
+++ app/src/main/java/com/example/mariana/projetomds/fragments/mapa/MapaActivity.java
@@ -1,18 +1,72 @@
 package com.example.mariana.projetomds.fragments.mapa;
 
+import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.mariana.projetomds.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapaActivity extends Fragment {
+import java.io.IOException;
+
+public class MapaActivity extends Fragment implements OnMapReadyCallback {
+//public class MapaActivity extends Fragment {
+
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.fragment_mapa, container, false);
-        return view;
+        mView = inflater.inflate(R.layout.fragment_mapa, container, false);
+        return mView;
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView = (MapView) mView.findViewById(R.id.map); // TODO: colocar na forma bind la
+        if(mMapView != null){
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+
+        }
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+
+        mGoogleMap = googleMap;
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(40.689247, -74.044502)).title("State of Liberty") .snippet("I hope to go there some day"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(-21.97972238, -47.88054228)).title("Melhor departamento") .snippet("O melhor departamento da UFSCar"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(-21.98002085, -47.87833214)).title("Observatório") .snippet("Lindo observar tudo"));
+
+        CameraPosition DC = CameraPosition.builder().target(new LatLng(-21.97972238, -47.88054228)).zoom(16).bearing(0).tilt(45).build();
+
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(DC));
     }
 }
